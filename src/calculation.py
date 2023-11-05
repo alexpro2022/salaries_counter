2af623279acd2a6399d791be62d4113ff5052cc7
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-11-05 18:46:32 +0300

    Passed tests version :-)

--- src/calculation.py
+++ src/calculation.py
@@ -12,25 +12,30 @@ def _get_stage_group_by(format: str) -> dict:
 
 
 def _get_data(start_date: dt, end_date: dt, group_type: str):
+    interval = (end_date - start_date).total_seconds()
+    hour = 60 * 60
+    day = hour * 24
+    month = day * 30.5
     match group_type:
         case 'hour':
             format = '%Y-%m-%dT%H'
             extra = ':00:00'
             time_sequence = [(
                 start_date + timedelta(hours=i)).strftime(format)
-                for i in range((end_date - start_date).days*24 + 1)]
+                for i in range(int(interval/hour) + 1)]
         case 'day':
             format = '%Y-%m-%d'
             extra = 'T00:00:00'
             time_sequence = [(
                 start_date + timedelta(days=i)).strftime(format)
-                for i in range((end_date - start_date).days + 1)]
+                for i in range(int(interval/day) + 1)]
         case 'month':
+            days_per_month = 28
             format = '%Y-%m'
             extra = '-01T00:00:00'
             time_sequence = [(
-                start_date + timedelta(days=i*30.5)).strftime(format)
-                for i in range(int((end_date-start_date).days/30))]
+                start_date + timedelta(days=i*days_per_month)).strftime(format)
+                for i in range(int(interval/month))]
     return _get_stage_group_by(format), time_sequence, extra
 
 
@@ -44,8 +49,7 @@ async def calc(collection: AgnosticCollection, data: dict[str, str]):
      time_sequence,
      extra) = _get_data(dt_from, dt_upto, group_type)
     stage_match_dates = {'$match': {'dt': {'$gte': dt_from, '$lte': dt_upto}}}
-    stage_sort = {'$sort': {'_id': 1}}
-    pipeline = [stage_match_dates, stage_group_and_sum, stage_sort]
+    pipeline = [stage_match_dates, stage_group_and_sum]
     initial_data = {time: 0 for time in time_sequence}
 
     async for item in collection.aggregate(pipeline):
